095ba5cb513c88045b16fc7521b2354fe2849ce3
wangcj <1261459491@qq.com>
2025-01-13 11:04:55 +0800

    first commit

---
+++ rtl/key_debounce.v
@@ -0,0 +1,33 @@
+`timescale 1ns / 1ps
+//****************************************VSCODE PLUG-IN**********************************//
+//----------------------------------------------------------------------------------------
+// IDE :                   VSCODE     
+// VSCODE plug-in version: Verilog-Hdl-Format-2.8.20240817
+// VSCODE plug-in author : 
+//----------------------------------------------------------------------------------------
+//****************************************Copyright (c)***********************************//
+// Copyright(C)            Please Write Company name
+// All rights reserved     
+// File name:              
+// Last modified Date:     2025/01/13 10:55:05
+// Last Version:           V1.0
+// Descriptions:           
+//----------------------------------------------------------------------------------------
+// Created by:             Please Write You Name 
+// Created date:           2025/01/13 10:55:05
+// mail      :             Please Write mail 
+// Version:                V1.0
+// TEXT NAME:              key_debounce.v
+// PATH:                   C:\Users\12614\Desktop\prj\key_debounce\rtl\key_debounce.v
+// Descriptions:           
+//                         
+//----------------------------------------------------------------------------------------
+//****************************************************************************************//
+
+module key_debounce(
+    input                               clk                        ,
+    input                               rst_n                      
+);
+                                                                   
+                                                                   
+endmodule
